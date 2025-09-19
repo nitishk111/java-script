9ec0b16402931ae9c51394542cda52038af6a40d
Nitish Chaudhary <84928231+nitishk111@users.noreply.github.com>
2025-09-19 10:49:09 +0000

    Events

---
+++ 08_Events/02_AsyncJS.js
@@ -0,0 +1,8 @@
+
+// Default Java Script is Synchronous and single threaded
+
+// Execution Context --> Executes code line by line; each opeation waits for prev opeation to complete..
+
+// => Blocking code vs Non Blocking code
+// Blocking code --> blocks flow of whole programme; ie. read file synchrounos
+// Non Blocking code --> Doesnt block execution; ie. read file async
